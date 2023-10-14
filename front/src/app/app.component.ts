390a0f3f134db0e772b6d57821dbf0bf34dd2826
rafadot <rafaelairesdemedeiros@outlook.com>
2023-10-13 23:43:50 -0300

    primeng

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front';
+
+  constructor(private primeConfig : PrimeNGConfig){}
+  
+  ngOnInit(){
+    this.primeConfig.ripple = true;
+  }
+
 }
